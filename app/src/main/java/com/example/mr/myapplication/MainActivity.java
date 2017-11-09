349517e8bd2b7545df79fc2b4470bcf4868a794c
TheDawnonly <zwwdyxo@163.com>
2017-11-09 10:09:32 +0800

    添加进度条

--- app/src/main/java/com/example/mr/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mr/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.mr.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -8,17 +10,21 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
     DownLoadFile downLoadFile;
     private String loadUrl = "http://gdown.baidu.com/data/wisegame/d2fbbc8e64990454/wangyiyunyinle_87.apk";
     private String filePath = Environment.getExternalStorageDirectory() + "/" + "网易云音乐.apk";
+    private ProgressBar progressBar;
+    private TextView tvprogress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ProgressBar progressBar = findViewById(R.id.main_pb);
-        final TextView tvprogress = findViewById(R.id.tv_progress);
+        progressBar = findViewById(R.id.main_pb);
+        tvprogress = findViewById(R.id.tv_progress);
         downLoadFile = new DownLoadFile(this, loadUrl, filePath, 3);
         downLoadFile.setOnDownLoadListener(new DownLoadFile.DownLoadListener() {
             @Override
@@ -30,7 +36,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onComplete() {
                 Toast.makeText(MainActivity.this, "下载完成", Toast.LENGTH_SHORT).show();
+                //下载完成后跳转到安装界面
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_VIEW);
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                i.setDataAndType(Uri.fromFile(new File(filePath)), "application/vnd.android.package-archive");
+                startActivity(i);
             }
+
 
             @Override
             public void onFailure() {
